fd97ca0f0516548d55e177d2f0b2a89599dde88e
Rupinder Pal Singh <rupinder.singh@smartenergywater.in>
2022-12-31 23:04:55 +0530

    Initial Commit

---
+++ HWSProject5/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  HWSProject5
+//
+//  Created by Rupinder on 01/03/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["someWord"]
+        }
+        
+    }
+    
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    
+
+
+}
+
